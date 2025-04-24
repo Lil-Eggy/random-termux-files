91cf2ae8ee741f4fc95ece22fa675641f65c3c93
Lil-Eggy <hafiq3255@gmail.com>
2025-04-24 20:30:13 +0800

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-#install packages and etc in termux
+#install packages in termux
 termux-change-repo
 termux-setup-storage
 pkg update
@@ -9,7 +9,7 @@ pkg install termux-x11-nightly
 pkg install pulseaudio
 pkg install proot-distro
 
-#install arch
+#install arch and creates a user called "lil_eggy"
 proot-distro install archlinux
 proot-distro login archlinux -c 'pacman -Sy && pacman -Syu && pacman -S sudo && pacman -S xfce4 && useradd -m -G wheel lil_eggy && passwd lil_eggy && nano /etc/sudoers && exit'
 
