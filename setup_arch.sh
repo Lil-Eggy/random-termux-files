0bd96d626d3e2d6584a991a44c441d6c288a1ced
Lil-Eggy <hafiq3255@gmail.com>
2025-04-25 19:12:46 +0800

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -8,13 +8,14 @@ pkg install x11-repo
 pkg install termux-x11-nightly
 pkg install pulseaudio
 pkg install proot-distro
+pkg install nano
 
 #install arch and creates a user called "lil_eggy"
 proot-distro install archlinux
 proot-distro login archlinux 
-pacman -Sy && pacman -Syu && pacman -S sudo && pacman -S xfce4 && useradd -m -G wheel lil_eggy && passwd lil_eggy && nano /etc/sudoers && exit
+pacman -Sy && pacman -Syu && pacman -S nano && pacman -S sudo && pacman -S xfce4 && useradd -m -G wheel lil_eggy && passwd lil_eggy && nano /etc/sudoers && exit
 
-#start arch and x11
+#download arch start script
 wget https://raw.githubusercontent.com/Lil-Eggy/random-termux-files/refs/heads/main/arch.sh
 chmod +x arch.sh
 echo now you can start arch by running ./arch.sh
