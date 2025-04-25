7040d659b50e710c097ca632d03d12aac78313e1
Lil-Eggy <hafiq3255@gmail.com>
2025-04-25 19:25:43 +0800

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -12,8 +12,7 @@ pkg install nano
 
 #install arch and creates a user called "lil_eggy"
 proot-distro install archlinux
-proot-distro login archlinux 
-pacman -Sy && pacman -Syu && pacman -S nano && pacman -S sudo && pacman -S xfce4 && useradd -m -G wheel lil_eggy && passwd lil_eggy && nano /etc/sudoers && exit
+proot-distro login archlinux -- /bin/bash -c 'pacman -Sy && pacman -Syu && pacman -S nano && pacman -S sudo && pacman -S xfce4 && useradd -m -G wheel lil_eggy && passwd lil_eggy && nano /etc/sudoers && exit'
 
 #download arch start script
 wget https://raw.githubusercontent.com/Lil-Eggy/random-termux-files/refs/heads/main/arch.sh
